48b9bf2d00255ec78a4fe6100f07637163608545
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-05-22 10:41:07 +0200

    Added val arrayOf for numbers

--- app/src/main/java/com/example/numberstorage/MainActivity.kt
+++ app/src/main/java/com/example/numberstorage/MainActivity.kt
@@ -12,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+    val numbers = arrayOf(1,2,3,4,5,6,7,8,9,10)
 
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
