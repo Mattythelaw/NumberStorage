1dc2932dd2d13893ec2a97a0441bc591c541bc56
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-05-27 10:23:43 +0200

    Deleted code for an updated version

--- app/src/main/java/com/example/numberstorage/MainActivity.kt
+++ app/src/main/java/com/example/numberstorage/MainActivity.kt
@@ -11,39 +11,15 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    val numbers = arrayOfNulls<Int>(10)
-    var counter = 10
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        //Binding the XML views to Kotlin//
-        val editNum = findViewById<EditText>(R.id.edtNum)
-        val editAdd = findViewById<Button>(R.id.edtAdd)
 
-        //Setting up the click listener for the button//
-        editAdd.setOnClickListener {
-            if (counter >= 10) {
-                Toast.makeText(
-                    this, "No more values can be stored",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                val inputText = editNum.text.toString()
 
-                if (inputText.isNotEmpty()) {
-                    val valueToStore = inputText.toInt()
-
-                    numbers[counter] = valueToStore
-
-                    //Incrementing the counter so the next click targets the next index//
-                    counter++
-                }
-
-            }
-        }
 
 
 
